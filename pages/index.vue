3674e89559f8f9b87cdd7dfe5ebd0c3161e534b2
dgwight <dylangwight@gmail.com>
2023-08-21 12:43:25 -0400

    feat: combobox for filters

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,51 @@
 <script>
-import { filter, includes, uniq } from "lodash";
+  import { filter, includes, uniq } from 'lodash'
 
-export default {
-  data() {
-    return {
-      clients: [],
-      keys: ["Name", "Title", "Quote", "Nationality"],
-      nationalities: [],
-      drawer: true,
-    };
-  },
-  computed: {
-    filteredClients() {
-      return filter(this.clients, (c) => {
-        const matchesNationality = this.nationalities.length
-          ? includes(this.nationalities, c.nationality)
-          : true;
-        const matchesTitle = this.nationalities.length
-          ? includes(this.nationalities, c.nationality)
-          : true;
-        return matchesNationality && matchesTitle;
-      });
+  export default {
+    data () {
+      return {
+        clients: [],
+        keys: ['Name', 'Title', 'Quote', 'Nationality'],
+        nationalities: [],
+        titles: [],
+        quotes: [],
+        drawer: true
+      }
     },
-    nationalityOptions() {
-      return uniq(this.clients.map((c) => c.nationality)).sort();
+    computed: {
+      filteredClients () {
+        return filter(this.clients, (c) => {
+          const matchesTitle = this.titles.length
+            ? includes(this.titles, c.title)
+            : true
+          const matchesNationality = this.nationalities.length
+            ? includes(this.nationalities, c.nationality)
+            : true
+          const matchesQuote= this.quotes.length
+            ? includes(this.quotes, c.quote)
+            : true
+          return matchesNationality && matchesTitle && matchesQuote
+        })
+      },
+      nationalityOptions () {
+        return uniq(this.clients.map((c) => c.nationality)).sort()
+      },
+      titleOptions () {
+        return uniq(this.clients.map((c) => c.title)).sort()
+      },
+      quoteOptions () {
+        return uniq(this.clients.map((c) => c.quote)).sort()
+      }
     },
-  },
-  created() {
-    this.getPosts();
-  },
-  methods: {
-    async getPosts() {
-      this.clients = await $fetch("/api/clients");
+    created () {
+      this.getPosts()
     },
-  },
-};
+    methods: {
+      async getPosts () {
+        this.clients = await $fetch('/api/clients')
+      }
+    }
+  }
 </script>
 
 <template>
@@ -42,63 +53,55 @@ export default {
     <v-layout class="rounded rounded-md">
       <v-app-bar color="primary">
         <v-app-bar-nav-icon
-          variant="text"
-          icon="mdi-filter"
-          @click.stop="drawer = !drawer"
+            variant="text"
+            icon="mdi-filter"
+            @click.stop="drawer = !drawer"
         ></v-app-bar-nav-icon>
 
         <v-toolbar-title>Clients</v-toolbar-title>
       </v-app-bar>
 
-      <v-navigation-drawer v-model="drawer">
-        <v-list>
-          <v-list-item title="Filters"></v-list-item>
+      <v-navigation-drawer v-model="drawer" class="py-4">
 
-          <v-text-field
-            clearable
-            class="mx-3 mt-3"
-            label="Name"
-            variant="underlined"
-          ></v-text-field>
-
-          <v-text-field
-            clearable
+        <v-combobox
+            label="Title"
+            :items="titleOptions"
+            v-model="titles"
             class="mx-4"
+            multiple
+            clearable
+        ></v-combobox>
+
+        <v-combobox
+            label="Nationality"
+            :items="nationalityOptions"
+            v-model="nationalities"
+            class="mx-4"
+            multiple
+            clearable
+            chips
+        ></v-combobox>
+
+        <v-combobox
             label="Quote"
-            variant="underlined"
-          ></v-text-field>
-
-          <v-list density="compact">
-            <v-list-subheader>Nationality</v-list-subheader>
-
-            <v-list-item
-              v-for="nationality in nationalityOptions"
-              :key="nationality"
-            >
-              <template #prepend>
-                <v-list-item-action start>
-                  <v-checkbox-btn
-                    v-model="nationalities"
-                    :value="nationality"
-                  ></v-checkbox-btn>
-                </v-list-item-action>
-              </template>
-              <v-list-item-title>{{ nationality || "None" }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-list>
+            :items="quoteOptions"
+            v-model="quotes"
+            class="mx-4"
+            multiple
+            clearable
+        ></v-combobox>
       </v-navigation-drawer>
 
       <v-main>
         <v-card class="mx-4 my-4">
           <v-list lines="two">
             <v-list-item
-              v-for="(client, i) in filteredClients"
-              :key="i"
-              :title="client.name"
-              :value="client"
-              :subtitle="client.title"
-              :prepend-avatar="client.avatar"
+                v-for="(client, i) in filteredClients"
+                :key="i"
+                :title="client.name"
+                :value="client"
+                :subtitle="client.title"
+                :prepend-avatar="client.avatar"
             >
             </v-list-item>
           </v-list>
